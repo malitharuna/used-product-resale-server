1ecc8ad441c8f800aee4505213c0d01df297157b
malitharuna <msmalitharuna@gmail.com>
2022-11-27 11:18:43 +0600

    index.js added

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.get('/', (req, res) => {
     res.send('Server is running')
 })
 app.listen(port, () => {
-    console.log('Server is running on', port);
+    console.log(`Server is running on ${port}`);
 })
