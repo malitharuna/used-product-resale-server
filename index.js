1b8ae53b5f3f83d097944f96b8a0b8fe5d60783a
malitharuna <msmalitharuna@gmail.com>
2022-11-28 08:50:04 +0600

    api created

--- index.js
+++ index.js
@@ -10,9 +10,57 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0b62e8i.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run () {
+    try{
+        const categoryCollection = client.db(`Desired-Wheel`).collection('category');
+        const productCollection = client.db(`Desired-Wheel`).collection('products');
+        const userCollection = client.db(`Desired-Wheel`).collection('Users');
+        
+
+        app.get("/category", async (req, res) => {
+            const query = {};
+            const categories = await categoryCollection.find(query).toArray();
+            res.send(categories);
+          });
+
+        app.get("/product/:brand", async (req, res) => {
+            const Brand = req.params.brand;
+            console.log(Brand)
+            const query = {
+                Brand
+            };
+
+            console.log(query)
+            
+            const result = await productCollection.find(query).toArray();
+            console.log(result)
+            res.send(result);
+
+          });
+
+          app.post('/addUser', async(req, res) =>{
+            const user = req.body;
+            console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
+
 app.get('/', (req, res) => {
     res.send('Server is running')
+
 })
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 })
+
